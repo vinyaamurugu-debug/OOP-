c79185475a8f677dc72159dc58de38614c85613e
vinyaamurugu-debug <vinyaamurugu@gmail.com>
2026-03-05 15:27:34 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes{
+    public static void main(String[] args) {
+
+        
+        byte b = 10;
+        short s = 200;
+        int i = 1000;
+        long l = 50000L;
+        float f = 5.5f;
+        double d = 10.99;
+        char c = 'V';
+        boolean flag = true;
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + flag);
+    }
+}
